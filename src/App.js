15264054c3d0b4b55ef79b98933adc8718c996da
Abdelrahman <abdelrahmanamrxi@gmail.com>
2024-07-17 14:59:41 +0300

    Changing stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import bolb from './images/blob.png'
+import bolb2 from './images/blob2.png'
+import React from 'react'
+import Questions from './Questions';
 function App() {
+  const [quiz,setquiz]=React.useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <main>
+      {
+   quiz?
+    <div>
+     <img className='bolb' src={bolb}></img>
+     <img className='bolb2' src={bolb2}></img>
+    <div className='Begin'>
+    <h1>Quizzical</h1>
+    <p className='description'>Some description if needed</p>
+    <button>Start Quiz</button>
     </div>
-  );
+    </div>
+    :
+    <div>
+       <img className='bolb' src={bolb}></img>
+       <img className='bolb2' src={bolb2}></img>
+      <div className='form'>
+      <Questions />
+      </div>
+    </div>
+}
+
+    </main>
+  )
 }
 
 export default App;
---
+++ src/Questions.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './questions.css'
+
+export default function Questions() {
+  return (
+    <div className='Question'>
+    <h3>How would one say goodbye in spanish?</h3>
+    </div>
+  )
+}
