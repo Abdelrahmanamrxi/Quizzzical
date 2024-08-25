af783869d0db34c668699254b93d20dbebe3a0f1
Abdelrahman Amr <abdelrahmanamrxi@gmail.com>
2024-08-25 16:30:39 +0300

    Uploading to Github

--- src/App.js
+++ src/App.js
@@ -1,32 +1,125 @@
 
 import './App.css';
+import './questions.css'
 import bolb from './images/blob.png'
 import bolb2 from './images/blob2.png'
 import React from 'react'
 import Questions from './Questions';
 function App() {
-  const [quiz,setquiz]=React.useState(false)
-  return (
+  const [questions,set_questions]=React.useState([])
+
+  const [quiz_page,setquiz]=React.useState(true)
+  const [isResultChecked,setResultChecked]=React.useState(false)
+  const[correct,set_correct]=React.useState(false)
   
+  async function Fetch(){
+    const response=await fetch('https://the-trivia-api.com/v2/questions')
+    const data=await response.json();
+    
+   
+    const modifiedData = data.map(question => {
+    
+      let answers = [
+        ...question.incorrectAnswers.map(answer => {
+          return { text: answer, id: Math.floor(Math.random() * 200000), checked: false ,question_id:question.id,correct:false};
+        }),
+        { text: question.correctAnswer, id: Math.floor(Math.random() * 200000), checked: false,question_id:question.id,correct:true }
+      ];
+
+      for (let i = answers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [answers[i], answers[j]] = [answers[j], answers[i]];
+      }
+
+      return { ...question, answers };
+    });
+
+    set_questions(modifiedData);
+  }
+
+  React.useEffect(() => {
+    
+    Fetch();
+      
+     
+    },[])
+
+    function HandleAnswer (answerId, questionId)  {
+      if (isResultChecked) return;
+      set_questions(prevQuestions =>
+        prevQuestions.map(question => {
+          if (question.id === questionId) {
+        
+            const updatedAnswers = question.answers.map(answer => ({
+              ...answer,
+              checked: answer.id === answerId
+
+              
+              
+            }));
+            return { ...question, answers: updatedAnswers };
+          }
+          return question;
+        })
+      );
+      
+    };
+
+function flipPage(){
+  setquiz(!quiz_page)
+}
+
+function Endgame() {
+  if (isResultChecked) {
+  Fetch();
+  
+    setResultChecked(false);
+    setquiz(true);
+  } else {
+  
+    setResultChecked(true);
+  }
+}
+
+  return (
+
     <main>
       {
-   quiz?
+   quiz_page?
     <div>
      <img className='bolb' src={bolb}></img>
-     <img className='bolb2' src={bolb2}></img>
+     <img className='bolbe2' src={bolb2}></img>
     <div className='Begin'>
     <h1>Quizzical</h1>
     <p className='description'>Some description if needed</p>
-    <button>Start Quiz</button>
+    <button onClick={flipPage} className='start'>Start Quiz</button>
     </div>
     </div>
     :
     <div>
-       <img className='bolb' src={bolb}></img>
-       <img className='bolb2' src={bolb2}></img>
+       <img className='bolb'  src={bolb}></img>
+      
       <div className='form'>
-      <Questions />
+      {questions.map((item)=>{
+        return <Questions key={questions.id} question={item.question.text}
+        Answers={item.answers}
+        id={item.answers.id}
+        answerid={item.id}
+        HandleAnswer={HandleAnswer}
+        checked={item.answers.checked}
+        correct={correct}
+        showResults={isResultChecked}
+        correctAnswer={item.correctAnswer}
+        
+
+     
+        />
+      })}
+     <button onClick={Endgame} className='End'>{isResultChecked?'Play Again':'Check Results'}</button>
+
+    
       </div>
+   
     </div>
 }
 
--- src/Questions.js
+++ src/Questions.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import './questions.css'
 
-export default function Questions() {
+export default function Questions(questions) {
+  function color(answer, showResults) {
+    let backgroundColor = '';
+ 
+  
+    if (showResults) {
+      if (answer.checked) {
+        backgroundColor = answer.correct ? '#6aa84f' : 'red'; 
+        
+      } else if (!answer.checked && answer.correct) {
+        backgroundColor = '#6aa84f';
+      }
+    }
+   
+    return backgroundColor;
+  }
   return (
     <div className='Question'>
-    <h3>How would one say goodbye in spanish?</h3>
+    <h3>{questions.question}</h3>
+    <div className='questions-layout'>
+      
+    {questions.Answers.map((item=>{
+        return <button key={item.id} style={{backgroundColor:color(item,questions.showResults)}} onClick={()=>{questions.HandleAnswer(item.id,item.question_id)}}
+         className={item.checked?'over':'options'} >{item.text}</button>
+    }))}
+   
+    
+    
+     </div>
+     <hr />
+     
+  
     </div>
   )
+
 }
